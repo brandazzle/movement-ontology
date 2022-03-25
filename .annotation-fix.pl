a5391e675de9203763a90f15b79508476e42d59f
Apprentice <brickmaster.lewis@gmail.com>
2022-03-25 15:09:50 -0700

    Fix annotation fixer

---
+++ .annotation-fix.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use feature 'unicode_strings';
+use Unicode::Normalize qw(NFD compose);
+
+$s = '';
+while (my $line = <>) {
+	$s = $s.$line;
+}
+$string = NFD($s);
+
+$string =~ s/(\>[^\n\t\r]+)\n\n([^\n\t\r]*)\s+(\*\s\*[\s\w\d]+?) on [^\n\t\r]+\n{0,2}/\1\n\n\2\n\3\*\n\n/img;
+
+$out = compose($string);
+print $out;
